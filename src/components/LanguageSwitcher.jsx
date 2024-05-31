772536b0424ba88342ffa99c881805898d63e2e7
Lucas Debeve <lucas.debeve2003@gmail.com>
2024-06-01 00:46:02 +0200

    Responsive du menu internationalization

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -15,7 +15,7 @@ function LanguageSwitcher() {
   }
 
   return (
-    <>
+    <div className="relative">
       <button type="button"
               className="py-1 px-3 text-xl text-gray-900 rounded hover:text-slate-800 transition-colors dark:text-gray-300 dark:hover:text-slate-50"
               onClick={toggleLanguageMenu}
@@ -24,13 +24,13 @@ function LanguageSwitcher() {
       </button>
       {/* dropdown menu*/}
       <div
-        className={`${isLanguageMenuOpen ? "block" : "hidden"} absolute top-12 right-10 w-52 bg-slate-100/50 rounded-2xl backdrop-blur-sm drop-shadow-sm border border-b-0 border-slate-50/50 dark:bg-neutral-800/50 dark:text-gray-300 dark:border-slate-50/20`}>
+        className={`${isLanguageMenuOpen ? "block" : "hidden"} absolute bottom-10 -right-12 bg-slate-100/50 w-36 rounded-2xl backdrop-blur-sm drop-shadow-sm border border-b-0 border-slate-50/50 sm:top-12 sm:right-0 sm:bottom-auto sm:w-52 dark:bg-neutral-800/50 dark:text-gray-300 dark:border-slate-50/20`}>
         {languages.map((lang, i) => (
           <LanguageButton key={`${lang}-${i}`} code={lang.code} name={lang.name} after={() => setIsLanguageMenuOpen(false)}/>
         ))
         }
       </div>
-    </>
+    </div>
   );
 }
 

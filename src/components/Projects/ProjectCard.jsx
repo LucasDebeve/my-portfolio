e8f2482c2851dc72ca0d4030a1eae65e7d38cd2f
Lucas Debeve <lucas.debeve2003@gmail.com>
2024-06-03 00:11:34 +0200

    Fix link

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -47,7 +47,7 @@ function ProjectCard({project}) {
             {project.links.map((link, index) => {
                 return (
                   <a href={link.url} key={`${project.title}-link-${index}`} target="_blank" rel="noreferrer"
-                     className={`w-full h-fit lg:w-auto text-center py-1 px-6 rounded-sm transition-colors ${link.primary ? "text-gray-300 bg-neutral-700 hover:text-gray-100 hover:bg-neutral-600" : "bg-neutral-200 text-neutral-700 border border-neutral-700 hover:text-neutral-900 hover:bg-neutral-300"} ${link.disabled && "pointer-events-none"} ${link.disabled && link.primary && "bg-neutral-400 text-neutral-500"} ${link.disabled && !link.primary && "border-neutral-400 text-neutral-400"}`}>
+                     className={`w-full h-fit lg:w-auto text-center border py-1 px-6 rounded-sm transition-colors ${link.primary ? (link.disabled ? "bg-neutral-400 text-neutral-500" : "text-gray-300 bg-neutral-700 hover:text-gray-100 hover:bg-neutral-600") : (link.disabled ? "border-neutral-400 text-neutral-400" : "bg-neutral-200 text-neutral-700 border-neutral-700 hover:text-neutral-900 hover:bg-neutral-300")} ${link.disabled && "pointer-events-none"}`}>
                     {link.title}
                   </a>
                 );

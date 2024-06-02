7026ce2251e9e1efa214e84a85190b149e6b0d58
Lucas Debeve <lucas.debeve2003@gmail.com>
2024-06-02 19:23:48 +0200

    Compétences dark Mode

--- src/components/Bento/BentoCell.jsx
+++ src/components/Bento/BentoCell.jsx
@@ -5,7 +5,7 @@ function BentoCell({ children, src = "", colSpan = false, rowSpan = false, class
   const [isSelected, setIsSelected] = useState(false);
 
   return (
-    <div className={`p-4 h-full flex flex-col justify-center items-center gap-2 ${colSpan && "lg:col-span-2"} ${rowSpan && "lg:row-span-2"} ${isSelected ? "bg-neutral-800 text-gray-300" : "bg-white text-gray-800"}  box-border border border-gray-800 hover:bg-neutral-800 hover:text-gray-300 hover:cursor-pointer transition-colors ${className}`}
+    <div className={`p-4 h-full flex flex-col justify-center items-center gap-2 ${colSpan && "lg:col-span-2"} ${rowSpan && "lg:row-span-2"} ${isSelected ? "bg-neutral-800 text-gray-300 dark:bg-gray-200 dark:text-neutral-800" : "bg-white text-gray-800"}  box-border border border-gray-800 hover:bg-neutral-800 hover:text-gray-300 hover:cursor-pointer dark:bg-neutral-800 dark:text-gray-300 dark:border-neutral-900 dark:hover:bg-gray-200 dark:hover:text-neutral-800 transition-colors ${className}`}
          id={id}
          onClick={() => setIsSelected(!isSelected)}
     >
--- src/components/Bento/BentoGrid.jsx
+++ src/components/Bento/BentoGrid.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 function BentoGrid({children, className = '', id = '', title = 'Grid'}) {
   return (
     <>
-      <h2 className="text-2xl scroll-mt-16 text-gray-800 dark:text-gray-300 select-none" id={id}>{title}</h2>
-      <div className={`grid lg:grid-flow-col grid-cols-2 sm:grid-cols-3 lg:grid-cols-8 lg:grid-rows-3 ${className}`}>
+      <h2 className="text-2xl scroll-mt-16 text-gray-800 dark:text-gray-300" id={id}>{title}</h2>
+      <div className={`grid lg:grid-flow-col grid-cols-2 sm:grid-cols-3 lg:grid-cols-8 lg:grid-rows-3  select-none ${className}`}>
         {children}
       </div>
     </>

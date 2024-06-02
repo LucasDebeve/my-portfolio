1c99bad9193080a0a835f980cc332984d03e8b51
Lucas Debeve <lucas.debeve2003@gmail.com>
2024-06-02 22:39:57 +0200

    Carte de projet

--- src/components/Filters/FilterItem.jsx
+++ src/components/Filters/FilterItem.jsx
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import {motion} from "framer-motion";
 
 function FilterItem({ filter, size = "text-sm" }) {
   return (
-    <div className={`bg-gray-300 text-gray-800 ${size} dark:bg-neutral-800 dark:text-gray-300 py-1 px-4 rounded-full whitespace-nowrap`}>
+    <motion.div className={`bg-gray-300 text-gray-800 ${size} dark:bg-neutral-800 dark:text-gray-300 py-1 px-4 rounded-full whitespace-nowrap`}
+                animate={{ opacity: [0, 1], scale: [0, 1], y: [30, 0] }}
+    >
       {filter}
-    </div>
+    </motion.div>
   );
 }
 
--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -2,31 +2,34 @@ import PropTypes from "prop-types";
 import FilterItem from "../Filters/FilterItem.jsx";
 import {motion} from "framer-motion";
 
-function ProjectCard({ project }) {
+function ProjectCard({project}) {
   return (
     <div className="w-full flex flex-col gap-2 border border-gray-300 rounded-sm text-gray-800">
-      <img src={project.image} alt={project.title} className="w-full h-32"/>
+      <img src={project.image} alt={project.title} className="w-full h-32 object-cover"/>
       <div className="w-full flex flex-col p-6 gap-3">
         <div className="flex justify-between">
           <h3 className="text-lg">{project.title}</h3>
           <p className="text-sm text-gray-600">{project.date}</p>
         </div>
         <p className="text-sm">{project.description}</p>
-        <div className="w-full inline-flex flex-nowrap gap-3">
+        <div className="flex items-center overflow-hidden gap-8 justify-start">
           {project.skills.map((skill, index) => {
             return (
-              <FilterItem key={`${project.title}-skill-${index}`} filter={skill} size="text-xs" />
+              <FilterItem key={`${project.title}-skill-${index}`} filter={skill} size="text-xs"/>
             );
           })}
         </div>
-        <div className="w-full flex flex-col-reverse gap-2">
+
+
+        <div className="w-full flex flex-col-reverse gap-2 lg:flex-row-reverse">
           {project.links.map((link, index) => {
-            return (
-              <a href={link.url} key={`${project.title}-link-${index}`} className={`w-full text-center p-1 rounded-sm transition-colors ${ link.primary ? "text-gray-300 bg-neutral-700 hover:text-gray-100 hover:bg-neutral-600" : "bg-neutral-200 text-neutral-700 border border-neutral-700 hover:text-neutral-900 hover:bg-neutral-300"}`}>
-                {link.title}
-              </a>
-            );
-          }
+              return (
+                <a href={link.url} key={`${project.title}-link-${index}`} target="_blank" rel="noreferrer"
+                   className={`w-full lg:w-auto text-center py-1 px-6 rounded-sm transition-colors ${link.primary ? "text-gray-300 bg-neutral-700 hover:text-gray-100 hover:bg-neutral-600" : "bg-neutral-200 text-neutral-700 border border-neutral-700 hover:text-neutral-900 hover:bg-neutral-300"}`}>
+                  {link.title}
+                </a>
+              );
+            }
           )}
         </div>
       </div>

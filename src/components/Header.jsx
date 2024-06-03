09d8283874ca3d5c5f773639ed3788501f61d62c
Lucas Debeve <lucas.debeve2003@gmail.com>
2024-06-03 21:27:45 +0200

    Fix menu de navigation sur mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,6 +13,11 @@ function Header() {
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+    if (isMenuOpen) {
+      document.body.classList.remove("overflow-y-hidden");
+    } else {
+      document.body.classList.add("overflow-y-hidden");
+    }
   };
 
   // set isMenuOpen to false when the window is resized
@@ -30,16 +35,18 @@ function Header() {
         <FontAwesomeIcon icon={faBars} className=" transition-colors text-2xl"/>
       </div>
       <nav
-        className={`${isMenuOpen ? "block" : "hidden"} sm:block fixed top-24 bottom-2 w-11/12 sm:bottom-auto sm:top-3 sm:w-auto bg-slate-100/50 px-5 py-2 rounded-2xl sm:rounded-full backdrop-blur-sm drop-shadow-sm border border-b-0 border-slate-50/50 dark:bg-neutral-800/50 dark:border-slate-50/20`}>
+        className={`${isMenuOpen ? "block" : "hidden"} sm:block fixed top-24 bottom-2 w-11/12 sm:bottom-auto sm:top-3 sm:w-auto bg-slate-100/50 px-5 py-2 z-50 rounded-2xl sm:rounded-full backdrop-blur-sm drop-shadow-sm border border-b-0 border-slate-50/50 dark:bg-neutral-800/50 dark:border-slate-50/20`}>
         <ul className="w-full h-full flex flex-col sm:flex-row justify-around sm:justify-center gap-5 items-center">
           <NavButton>{t("header.home")}</NavButton>
           <NavButton link="#timeline">{t("header.xp")}</NavButton>
           <NavButton link="#skills">{t("header.skills")}</NavButton>
           <NavButton link="#projects">{t("header.projects")}</NavButton>
-          {isMenuOpen && <li className="flex gap-12">
-            <LanguageSwitcher/>
-            <DarkModeSwitcher/>
-          </li>}
+          {isMenuOpen && (
+            <li className="flex gap-12">
+              <LanguageSwitcher/>
+              <DarkModeSwitcher/>
+            </li>
+          )}
         </ul>
       </nav>
       <div className="hidden sm:flex sm:flex-col-reverse md:flex-row fixed top-3 right-5 gap-3">

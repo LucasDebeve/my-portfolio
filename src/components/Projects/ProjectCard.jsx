7602b2a9a5175886d20b91924a4e16d532509634
Lucas Debeve <lucas.debeve2003@gmail.com>
2024-06-03 13:50:20 +0200

    Dark mode réalisations

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -23,17 +23,17 @@ function ProjectCard({project}) {
   };
   return (
       <motion.div
-        className="w-full flex flex-col gap-2 border border-gray-300 rounded-sm text-gray-800 md:flex-row md:h-72"
+        className="w-full flex flex-col gap-2 border border-gray-300 rounded-sm text-gray-800 md:flex-row md:h-72 dark:border-neutral-700 dark:bg-neutral-800"
         variants={animeVariants}
         initial="hidden"
         whileInView="visible"
         exit="hidden"
       >
         <ClickableImage src={project.image} alt={project.title} />
-        <div className="w-full flex flex-col p-6 gap-3">
+        <div className="w-full flex flex-col p-6 gap-3 dark:text-gray-300">
           <div className="flex justify-between">
             <h3 className="text-lg">{project.title}</h3>
-            <p className="text-sm text-gray-600">{project.date}</p>
+            <p className="text-sm text-gray-600  dark:text-gray-400">{project.date}</p>
           </div>
           <p className="text-sm">{project.description}</p>
           <div className="flex flex-nowrap gap-5 overflow-hidden">
@@ -47,7 +47,7 @@ function ProjectCard({project}) {
             {project.links.map((link, index) => {
                 return (
                   <a href={link.url} key={`${project.title}-link-${index}`} target="_blank" rel="noreferrer"
-                     className={`w-full h-fit lg:w-auto text-center border py-1 px-6 rounded-sm transition-colors ${link.primary ? (link.disabled ? "bg-neutral-400 text-neutral-500" : "text-gray-300 bg-neutral-700 hover:text-gray-100 hover:bg-neutral-600") : (link.disabled ? "border-neutral-400 text-neutral-400" : "bg-neutral-200 text-neutral-700 border-neutral-700 hover:text-neutral-900 hover:bg-neutral-300")} ${link.disabled && "pointer-events-none"}`}>
+                     className={`w-full h-fit lg:w-auto text-center border py-1 px-6 rounded-sm transition-colors ${link.primary ? (link.disabled ? "bg-neutral-400 text-neutral-500 dark:bg-neutral-800 dark:border-neutral-900" : "text-gray-300 bg-neutral-700 hover:text-gray-100 hover:bg-neutral-600 dark:bg-neutral-900 dark:border-neutral-900 dark:hover:bg-neutral-700") : (link.disabled ? "border-neutral-400 text-neutral-400 dark:border-neutral-600" : "bg-neutral-200 text-neutral-700 border-neutral-700 hover:text-neutral-900 hover:bg-neutral-300 dark:bg-neutral-800 dark:text-gray-300 dark:border-gray-400 dark:hover:bg-neutral-700")} ${link.disabled && "pointer-events-none"}`}>
                     {link.title}
                   </a>
                 );

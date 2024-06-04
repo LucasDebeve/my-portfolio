e6c8c2607b7ba1f69d5aafd9d017f0d71b3d2813
Lucas Debeve <lucas.debeve2003@gmail.com>
2024-06-04 18:53:33 +0200

    Fix : header flou

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,7 +35,7 @@ function Header() {
         <FontAwesomeIcon icon={faBars} className=" transition-colors text-2xl"/>
       </div>
       <nav
-        className={`${isMenuOpen ? "block" : "hidden"} sm:block fixed top-24 bottom-2 w-11/12 sm:bottom-auto sm:top-3 sm:w-auto bg-slate-100/50 px-5 py-2 z-50 rounded-2xl sm:rounded-full backdrop-blur-sm drop-shadow-sm border border-b-0 border-slate-50/50 dark:bg-neutral-800/50 dark:border-slate-50/20`}>
+        className={`${isMenuOpen ? "block" : "hidden"} sm:block fixed top-24 bottom-2 w-11/12 sm:bottom-auto sm:top-3 sm:w-auto bg-slate-100/50 px-5 py-2 z-50 rounded-2xl sm:rounded-full backdrop-blur-md sm:backdrop-blur-sm drop-shadow-sm border border-b-0 border-slate-50/50 dark:bg-neutral-800/50 dark:border-slate-50/20`}>
         <ul className="w-full h-full flex flex-col sm:flex-row justify-around sm:justify-center gap-5 items-center">
           <NavButton>{t("header.home")}</NavButton>
           <NavButton link="#timeline">{t("header.xp")}</NavButton>
